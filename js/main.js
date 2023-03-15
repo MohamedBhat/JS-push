4de88322266e392f7286425cf40d3fcdca2d730e
MohamedBhat <hamadabhat@gmail.com>
2023-03-15 17:41:15 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,97 @@
+let body = document.querySelector("body");
+let title = document.getElementById("title");
+let cost = document.querySelectorAll("#cost input");
+let total = document.getElementById("total");
+let count = document.getElementById("count");
+let department = document.getElementById("department");
+let btncreat = document.getElementById("btncreat");
+let tbody = document.getElementById("tbody")
+
+
+let alldata;
+if(localStorage.product !=null){
+    alldata = JSON.parse(localStorage.product)
+}else{
+    alldata =[];
+}
+
+
+
+// total value
+let getTotal = ()=>{
+
+    let price = cost[0].value,
+        taxes = cost[1].value,
+        tCost = cost[2].value,
+        discount = cost[3].value;
+        total = cost[4].value
+
+    let gettotalvalue =  (+price + +taxes + +tCost)- discount
+
+    cost[4].value = Math.ceil(gettotalvalue) ; 
+}
+
+for (let i = 0 ; i < cost.length ; i++){
+
+    cost[i].addEventListener('keyup' , getTotal)
+}
+
+
+let creatobject = ()=>{
+    let newdata = {
+        title : title.value,
+        price : cost[0].value,
+        taxes : cost[1].value,
+        tCost : cost[2].value,
+        discount : cost[3].value,
+        total: cost[4].value,
+        department : department.value
+    }
+
+    alldata.push(newdata);
+    localStorage.setItem("product" , JSON.stringify(alldata));
+    showdata();
+
+}
+
+
+
+
+//show data
+let showdata = ()=>{
+let table='';
+
+    for(let i =0 ; i< alldata.length ; i++){
+
+        table += `
+        <tr>
+            <td>${i}</td>
+            <td>${alldata[i].title}</td>
+            <td>${alldata[i].price}</td>
+            <td>${alldata[i].taxes}</td>
+            <td>${alldata[i].tCost}</td>
+            <td>${alldata[i].discount}</td>
+            <td>${alldata[i].total}</td>
+            <td>${alldata[i].department}</td>
+        </tr>
+        `
+    }
+
+    tbody.innerHTML = table;
+
+}
+
+showdata();
+btncreat.addEventListener("click" , creatobject);
+
+
+
+
+
+
+// clear inputs
+
+
+
+
+
